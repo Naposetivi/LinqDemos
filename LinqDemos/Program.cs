400af706dd2e1bf197425550831a5d7cce5198a3
Naposetivi <110626741+Naposetivi@users.noreply.github.com>
2023-09-13 17:53:42 +0300

    all the code from the 4th session on Parallel was added to the Program.cs file

--- LinqDemos/Program.cs
+++ LinqDemos/Program.cs
@@ -6,21 +6,146 @@ namespace LinqDemos
     [TestFixture]
     class Program
     {
-        [Test]
-        public void Test()
-        {
-            var a = new[] { 1, 2, 3 };
-            var b = new List<int> { 1, 2, 3 };
+        //[Test]
+        //public void Test()
+        //{
+        //    var a = new[] { 1, 2, 3 };
+        //    var b = new List<int> { 1, 2, 3 };
 
-            Assert.That(a, Is.EqualTo(b));
+        //    Assert.That(a, Is.EqualTo(b));
 
-            var c = new List<int> { 3, 2, 1 };
-            Assert.That(() => a, Is.EquivalentTo(c));
-        }
+        //    var c = new List<int> { 3, 2, 1 };
+        //    Assert.That(() => a, Is.EquivalentTo(c));
+        //}
 
 
         static void Main(string[] args)
         {
+
+            // --------------- Parallels ---------------
+
+
+
+            // --- AsParallel and ParallelQuery ---
+
+
+            //const int count = 50;
+
+            //var items = Enumerable.Range(1, count).ToArray();
+            //var results = new int[count];
+
+            //items.AsParallel().ForAll(x =>
+            //{
+            //    int newValue = x * x * x;
+            //    Console.Write($"{newValue} ({Task.CurrentId})\t");
+            //    results[x - 1] = newValue;
+            //});
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //foreach(var i in results)
+            //{
+            //    Console.WriteLine($"{i}\t");
+            //}
+            //Console.WriteLine();
+
+            //var cubes = items.AsParallel().AsOrdered().Select(x => x * x * x);
+
+            //var arr = cubes.ToArray();
+            //foreach(var i in cubes)
+            //    Console.Write($"{i}\t");
+            //Console.WriteLine();
+
+
+
+            // --- Cancellation and Exception ---
+
+
+            //var cts = new CancellationTokenSource();
+
+            //var items = ParallelEnumerable.Range(1, 20);
+
+            //var results = items.WithCancellation(cts.Token).Select(i =>
+            //{
+            //    double result = Math.Log10(i);
+
+            //    //if (result > 1) throw new InvalidOperationException();
+
+            //    Console.WriteLine($"i = {i}, tid = {Task.CurrentId}");
+            //    return result;
+            //});
+
+            //try
+            //{
+            //    foreach (var c in results)
+            //    {
+            //        if(c > 1)
+            //        {
+            //            cts.Cancel();
+            //        }
+            //        Console.WriteLine($"result = {c}");
+            //    }
+            //}
+            //catch (AggregateException ae)
+            //{
+            //    ae.Handle(e =>
+            //    { 
+            //        Console.WriteLine($"{e.GetType().Name}: {e.Message}");
+            //        return true;
+            //    });
+            //}
+            //catch (OperationCanceledException)
+            //{
+            //    Console.WriteLine("Canceled");
+            //}
+
+
+
+            // --- Merge Options ---
+
+
+            //var numbers = Enumerable.Range(1, 20).ToArray();
+
+            //var results = numbers
+            //    .AsParallel()
+            //    .WithMergeOptions(ParallelMergeOptions.FullyBuffered)
+            //    .Select(x => 
+            //    {
+            //        var result = Math.Log10(x);
+            //        Console.Write($"P {result}\t");
+            //        return result;
+            //    });
+
+            //foreach ( var result in results )
+            //{
+            //    Console.Write($"C {result}\t");
+            //}
+
+
+
+            // --- Custom Aggregation ---
+
+
+            //var sum = Enumerable.Range(1, 1000).Sum();
+
+            //var sum = Enumerable.Range(1, 1000)
+            //    .Aggregate(0, (i, acc) => i + acc);
+
+            //var sum = ParallelEnumerable.Range(1, 1000)
+            //      .Aggregate(
+            //        0,
+            //        (partialSum, i) => partialSum += i,
+            //        (total, subtotal) => total += subtotal,
+            //        i => i);
+
+            //Console.WriteLine("Sum = " + sum);
+
+
+            // -------------------------------------------
+
+
+
             //var p = new Params(1, 2, 3);
 
             //foreach (var item in p)
